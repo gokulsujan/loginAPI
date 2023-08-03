e072cdc04a497e9fbba6b0131bfb871a018dbba9
Gokul Sujan <gokulsujan03@gmail.com>
2023-08-03 11:35:19 +0530

    Login function completed

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
+
+var LoginData struct {
+	UserName string `json:"username"`
+	Password string `json:"password"`
+}
+
+func loginHandler(c *gin.Context) {
+	if err := c.ShouldBindJSON(&LoginData); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	const validUserName = "superadmin"
+	const validPassword = "superpassword"
+
+	if LoginData.UserName == validUserName && LoginData.Password == validPassword {
+		c.JSON(200, gin.H{"message": "Login successful"})
+	} else {
+		c.JSON(401, gin.H{"message": "Invalid Credentials"})
+	}
+}
 
 func main() {
 	r := gin.Default()
+	r.POST("/login", loginHandler)
 	r.Run(":5050")
 }
